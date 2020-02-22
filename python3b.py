bc94a6475a451046075e3bc93eccbf1b7d3fe17f
fwsmit <fw.smit01@gmail.com>
2020-02-22 12:28:31 +0100

    finished 3b

--- python3b.py
+++ python3b.py
@@ -3,20 +3,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-g = 9.81 # m/s^2
+#  g = 9.81 # m/s^2
+g = 0 # m/s^2
 
 mass            = 12    # kg
 deltaTime 	= 0.001 	#size of the time step (in seconds)
 beginTime       = 0 	#start time of simulation (in seconds)
 endTime 	= 10 	#end time of simulation (in seconds)
 beginPos 	= 0.2 	#initial position (in meters)
-beginVel 	= 0.1 	#initial velocity (in meters/second)
+beginVel 	= 0.6 	#initial velocity (in meters/second)
 duration        = endTime - beginTime
 times           = np.linspace(beginTime, endTime, 1 + round((duration)/deltaTime))
 x_an            = np.zeros(len(times))
 v_an            = np.zeros(len(times))
 x_num           = np.zeros(len(times))
 v_num           = np.zeros(len(times))
+spring_k        = 250   # spring stiffness (Newton/meter)
         
 # initialize numerical vectors with begin values
 x_num[0] = beginPos
@@ -33,7 +35,7 @@ v_num[0] = beginVel
 def derivatives(state, t): #function name is free to choose, but it must take exactly these two inputs
     y = state[0] #this will seem useless for now, but it is best to always unpack your states here
     v = state[1] #same as above, but a little less useless because we will provide v as a return value
-    a = g        #you will modify this line several times during this assignment. For now: constant a
+    a = - spring_k * y /mass        #you will modify this line several times during this assignment. For now: constant a
     return [v, a]
 
 def getXNum(t):
@@ -45,9 +47,13 @@ def calculateNumerical():
         # calculate new states based on old states and old derivatives
 
         # notice how the two states (y and v) are treated exactly the same
+        if np.sign(x_num[n-1]) != np.sign( x_num[n] ):
+            print("Y is zero")
+            print(times[n])
 
         x_num[n+1] = x_num[n] + afgeleiden[0]*deltaTime
         v_num[n+1] = v_num[n] + afgeleiden[1]*deltaTime 
+        
 
     #  def getVelocityAn(self, t):
         #  return (200*t/self.mass + 1/100)**-0.5
@@ -79,7 +85,7 @@ def plotPositionAn():
 
 def printAnswers():
     print("Answers:")
-    print(getXNum(10))
+    print(x_num[-1])
     #  print(self.getPositionAn(8)-self.x_num[8000])
 
 
@@ -90,6 +96,6 @@ printAnswers()
 #  test.plotPositionAn()
 #  test.plotVel()
 #  test.plotVelAn()
-#  plotPosition()
+plotPosition()
 plt.show()
 
